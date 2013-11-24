87a77887f7aaeefbd2109aca3bfae626a61d2b90
malintha <malinthak2@gmail.com>
2013-11-24 09:20:20 +0530

    interpretter ajax added

---
+++ src/main/java/manipulate/validateInput.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manipulate;
+
+/**
+ *
+ * @author Administrator
+ */
+public class validateInput {
+    
+    private String input=null;
+    private String[] words;
+    
+    public validateInput(String input){
+    this.input=input;
+    }
+    
+    public boolean getValidity(){
+    
+      for(String word :input.split(" ")){
+      words=new String[input.length()];
+      
+      
+      }
+          
+        
+        return false;
+    }
+    
+}
